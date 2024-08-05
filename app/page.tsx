52fd16dc1ca96875ff40f51589fba731f84f903c
tx-nozaki <nozaki@tv-tokyo.co.jp>
2024-08-05 21:54:50 +0900

    added per-user data isolation

--- app/page.tsx
+++ app/page.tsx
@@ -37,12 +37,11 @@ export default function App() {
     client.models.Todo.delete({ id })
   }
 
-  return (
-        
+  return (    
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>My todos</h1>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
